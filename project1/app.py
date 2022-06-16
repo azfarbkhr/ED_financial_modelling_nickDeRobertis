bd91339eaf81b9039f9ea148ed33e5173453ce3d
azfarbkhr <azfarbkhr@gmail.com>
2022-06-16 14:03:57 +0500

    added a button for calculate

--- project1/app.py
+++ project1/app.py
@@ -5,8 +5,7 @@ import matplotlib.ticker as ticker
 
 st.set_page_config(layout="wide")
 
-st.title("Az Retirement Calculator")
-
+confirm_button = st.sidebar.button("Calculate")
 starting_salary = st.sidebar.slider('Starting Salary', value=60000, min_value=0, max_value=1000000, step=1000)
 promos_every_n_years = st.sidebar.number_input('Promos Every N Years', value=5)
 cost_of_living_raise = st.sidebar.number_input('Cost of Living Raise', value=0.02)
@@ -15,29 +14,37 @@ savings_rate = st.sidebar.number_input('Savings Rate', value=0.25)
 interest_rate = st.sidebar.number_input('Interest Rate', value=0.05)
 desired_cash = st.sidebar.slider('Desired Cash', value=1500000, min_value=0, max_value=10000000, step=1000000)
 
+
 model_data = ModelInputs(starting_salary, promos_every_n_years, cost_of_living_raise, promo_raise, savings_rate, interest_rate, desired_cash)
 
 st.write("### Model Outputs")
-years, wealth_predictions = years_to_retirement(model_data)
 
-st.write(f'It will take {years} years to reach PKR.{desired_cash:,.0f} of wealth.')
+if confirm_button:
+    st.title("Az Retirement Calculator")
 
-"#### Visualizations"
-fig, ax = plt.subplots(figsize=(4, 2))
-ax.plot(wealth_predictions.index, wealth_predictions['Wealth'])
-plt.xticks(rotation=90)
-ax.plot((0, years), (desired_cash,desired_cash), '--', color='red')
+    years, wealth_predictions = years_to_retirement(model_data)
+    
+    old_years, old_wealth_predictions = years, wealth_predictions
 
-scale_y = 1000000
-ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y))
+    st.write(f'It will take {years} years to reach PKR.{desired_cash:,.0f} of wealth.')
 
-ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y))
-ax.yaxis.set_major_formatter(ticks_y)
-ax.set_ylabel('Wealth (val in millions)')
-ax.set_xlabel('Years')
-st.pyplot(fig)
+    "#### Visualizations"
+    fig, ax = plt.subplots(figsize=(4, 2))
+    ax.plot(wealth_predictions.index, wealth_predictions['Wealth'])
+    plt.xticks(rotation=90)
+    ax.plot((0, years), (desired_cash,desired_cash), '--', color='red')
+
+    scale_y = 1000000
+    ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y))
+
+    ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y))
+    ax.yaxis.set_major_formatter(ticks_y)
+    ax.set_ylabel('Wealth (val in millions)')
+    ax.set_xlabel('Years')
+    st.pyplot(fig)
 
 
-st.write("#### Prediction Details")
-st.dataframe(wealth_predictions[['Annual Salary', 'Cash Saved', 'Wealth']])
-
+    st.write("#### Prediction Details")
+    st.dataframe(wealth_predictions[['Annual Salary', 'Cash Saved', 'Wealth']])
+else:
+    st.write("*Select your parameters and click on the calculate button from the side menu.")
--- project1/retirement_model.py
+++ project1/retirement_model.py
@@ -4,7 +4,7 @@ import pandas as pd
 @dataclass
 class ModelInputs:
     starting_salary: int = 28000
-    promos_every_n_years: int = 5
+    promos_every_n_years: float = 5.0
     cost_of_living_raise: float = 0.02
     promo_raise: float = 0.15
     savings_rate: float = 0.50
@@ -42,14 +42,14 @@ def years_to_retirement(data):
     prior_wealth = 0
     wealth = 0
 
-    print("calculating years to retirement")   
+    print("calculating years to retirement..")   
     wealth_predictions = pd.DataFrame(columns=['Wealth', 'Year', 'Annual Salary', 'Cash Saved'])
     wealth_predictions.index.name = 'year'
     while wealth < data.desired_cash:
         year += 1
         wealth = wealth_at_year(data, year, prior_wealth)
-        print(wealth < data.desired_cash)
-        print(f"{wealth:,.0f}")
+        #print(wealth < data.desired_cash)
+        #print(f"{wealth:,.0f}")
         prior_wealth = wealth
         wealth_predictions.loc[year] = [wealth, year, salary_at_year(data, year), cash_saved_during_year(data, year)]
     return (year, wealth_predictions)
